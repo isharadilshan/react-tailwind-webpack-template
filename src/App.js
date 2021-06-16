967fe681c9b91f2f4fa733f7021adef286dee72d
Ishara Dilshan <isharadilshandev@gmail.com>
2021-06-16 16:40:04 +0530

    task: added google analytics

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import {BrowserRouter, useHistory} from 'react-router-dom';
 import {Auth0Provider} from '@auth0/auth0-react';
+import ReactGA from 'react-ga';
+import {createBrowserHistory} from 'history';
 import {ToastProvider} from 'react-toast-notifications';
 import {Provider as AppProvider} from './context/AppContext';
 import AppRoutes from './routes/AppRoutes';
 import './app.scss';
 import './custom.css';
+
+ReactGA.initialize('UA-177375203-1');
+
+const history = createBrowserHistory();
+history.listen((location) => {
+  ReactGA.set({page: location.pathname});
+  ReactGA.pageview(location.pathname);
+});
 
 const App = () => {
   const domain = process.env.REACT_APP_AUTH0_DOMAIN;
